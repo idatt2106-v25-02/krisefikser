daee9b5ca1c08e49ec59bdcf5d15c14660beaaf3
Jakob Huuse <jakob.huuse@gmail.com>
2025-04-22 12:13:18 +0200

    Initialization of project

---
+++ backend/src/test/java/stud/ntnu/krisefikser/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.ntnu.krisefikser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
