302c3925dfabee09196b8588e614b92797e54a84
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-04-25 12:44:56 +0200

    fix: add closing tags

--- frontend/src/components/layout/Navbar.vue
+++ frontend/src/components/layout/Navbar.vue
@@ -1,51 +1,61 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Map as MapIcon, Home, Package, Menu as MenuIcon, X, LogIn, User } from 'lucide-vue-next'
+import { RouterLink } from 'vue-router'
+
+const isMenuOpen = ref(false)
+</script>
+
 <template>
   <nav class="bg-white shadow-sm sticky top-0 z-50">
     <div class="container mx-auto px-4 py-3">
       <div class="flex justify-between items-center">
         <div class="flex items-center">
-          <router-link to="/" class="flex items-center">
+          <RouterLink to="/" class="flex items-center">
             <img src="@/assets/logo.svg" alt="Krisefikser.no" class="h-8 w-auto mr-2" />
             <span class="text-xl font-bold text-blue-700">Krisefikser.no</span>
-          </router-link>
+          </RouterLink>
         </div>
 
         <div class="hidden md:flex items-center space-x-8">
-          <router-link
+          <RouterLink
             to="/kart"
             class="flex items-center text-gray-700 hover:text-blue-600 transition"
           >
             <MapIcon class="h-5 w-5 mr-1" />
             <span>Kart</span>
-          </router-link>
-          <router-link
+          </RouterLink>
+          <RouterLink
             to="/husstand"
             class="flex items-center text-gray-700 hover:text-blue-600 transition"
           >
             <Home class="h-5 w-5 mr-1" />
             <span>Husstand</span>
-          </router-link>
+          </RouterLink>
 
-          <router-link to="/husstand/:id/beredskapslager" class="flex items-center text-gray-700 hover:text-blue-600 transition">
-          
-          <router-link
+          <RouterLink
+            to="/husstand/:id/beredskapslager"
+            class="flex items-center text-gray-700 hover:text-blue-600 transition"
+          ></RouterLink>
+
+          <RouterLink
             to="/legg-til-vare"
             class="flex items-center text-gray-700 hover:text-blue-600 transition"
           >
-
             <Package class="h-5 w-5 mr-1" />
             <span>Beredskapslager</span>
-          </router-link>
-          <router-link
+          </RouterLink>
+          <RouterLink
             to="/dashboard"
             class="flex items-center text-gray-700 hover:text-blue-600 transition"
           >
             <User class="h-5 w-5 mr-1" />
             <span>Min profil</span>
-          </router-link>
-          <router-link
+          </RouterLink>
+          <RouterLink
             to="/logg-inn"
             class="bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700 transition ml-2"
-            >Logg inn</router-link
+            >Logg inn</RouterLink
           >
         </div>
 
@@ -77,13 +87,15 @@
           <span>Husstand</span>
         </router-link>
 
-        <router-link to="/husstand/:id/beredskapslager" class="flex items-center px-3 py-2 rounded text-gray-700 hover:bg-gray-200">
+        <router-link
+          to="/husstand/:id/beredskapslager"
+          class="flex items-center px-3 py-2 rounded text-gray-700 hover:bg-gray-200"
+        ></router-link>
 
         <router-link
           to="/legg-til-vare"
           class="flex items-center px-3 py-2 rounded text-gray-700 hover:bg-gray-200"
         >
-
           <Package class="h-5 w-5 mr-2" />
           <span>Beredskapslager</span>
         </router-link>
@@ -105,25 +117,3 @@
     </div>
   </nav>
 </template>
-
-<script lang="ts">
-import { Map as MapIcon, Home, Package, Menu as MenuIcon, X, LogIn, User } from 'lucide-vue-next'
-
-export default {
-  name: 'AppNavbar',
-  components: {
-    MapIcon,
-    Home,
-    Package,
-    MenuIcon,
-    X,
-    LogIn,
-    User,
-  },
-  data() {
-    return {
-      isMenuOpen: false,
-    }
-  },
-}
-</script>
