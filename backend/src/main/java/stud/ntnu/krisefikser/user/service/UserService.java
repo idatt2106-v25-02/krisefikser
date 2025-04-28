5674f82cc9eae4d36d8564d5e81f83e70a074426
kdm-kev-NTNU <kevindm@stud.ntnu.no>
2025-04-28 13:11:37 +0200

    task - connection of auth service with user serivce based on the currenet email implementation

--- backend/src/main/java/stud/ntnu/krisefikser/user/service/UserService.java
+++ backend/src/main/java/stud/ntnu/krisefikser/user/service/UserService.java
@@ -12,6 +12,7 @@ import stud.ntnu.krisefikser.auth.entity.Role.RoleType;
 import stud.ntnu.krisefikser.auth.exception.RoleNotFoundException;
 import stud.ntnu.krisefikser.auth.repository.RoleRepository;
 import stud.ntnu.krisefikser.user.dto.CreateUserDto;
+import stud.ntnu.krisefikser.user.dto.UserDto;
 import stud.ntnu.krisefikser.user.entity.User;
 import stud.ntnu.krisefikser.user.exception.EmailAlreadyExistsException;
 import stud.ntnu.krisefikser.user.exception.UserDoesNotExistException;
@@ -29,11 +30,15 @@ public class UserService {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     String email = authentication.getName();
 
+    return getUserByEmail(email);
+  }
+
+  public User getUserByEmail(String email) {
     return userRepository.findByEmail(email).orElseThrow(
         () -> new UserDoesNotExistException("User with email " + email + " does not exist"));
   }
 
-  public User createUser(CreateUserDto data) {
+  public UserDto createUser(CreateUserDto data) {
     if (userRepository.existsByEmail(data.getEmail())) {
       throw new EmailAlreadyExistsException(
           "User with email " + data.getEmail() + " already exists");
@@ -45,9 +50,15 @@ public class UserService {
     Set<Role> roles = new HashSet<>();
     roles.add(userRole);
 
-    User user = User.builder().email(data.getEmail())
-        .password(passwordEncoder.encode(data.getPassword())).roles(roles).build();
+    User user = User.builder()
+        .email(data.getEmail())
+        .password(passwordEncoder.encode(data.getPassword()))
+        .firstName(data.getFirstName())
+        .lastName(data.getLastName())
+        .roles(roles)
+        .build();
 
-    return userRepository.save(user);
+    User savedUser = userRepository.save(user);
+    return savedUser.toDto();
   }
 }
