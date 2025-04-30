787efa4db568de194cdee7e376b838a373a3f90b
Embret Roås <44099156+Embretr@users.noreply.github.com>
2025-04-30 13:40:49 +0200

    feat/dockerized (#204)
    * dockerized and added dynamic frontend url
    
    * modified pipeline slightly
    
    * updated test properties
    
    * added frontend config to test security config
    
    * made .env file optional
    
    * remove .idea
    
    * feat: dockerize frontend
    
    * fix conflicts
    
    * fix problems with orval generaiton in frontend

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'orval'
+import { config } from 'dotenv'
+
+config()
+
+const openApiUrl = process.env.VITE_API_URL ? `${process.env.VITE_API_URL}/v3/api-docs` : './openapi.json'
 
 export default defineConfig({
   krisefikser: {
@@ -10,7 +15,7 @@ export default defineConfig({
       mock: false,
       prettier: true,
       clean: false,
-      baseUrl: 'http://localhost:8080',
+      baseUrl: process.env.VITE_API_URL || 'http://localhost:8080',
       override: {
         mutator: {
           path: './src/api/axios.ts',
@@ -19,7 +24,7 @@ export default defineConfig({
       },
     },
     input: {
-      target: 'http://localhost:8080/v3/api-docs',
+      target: openApiUrl,
     },
   },
 })
