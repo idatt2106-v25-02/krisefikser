34b47a2e6b2fb1a40d1a013b32b3309eaabfdc7b
Kaamya Shinde <kaamyashinde@gmail.com>
2025-04-30 14:19:19 +0200

    style: add formating changes

--- frontend/src/views/nonRegistered/RegisterView.vue
+++ frontend/src/views/nonRegistered/RegisterView.vue
@@ -97,14 +97,14 @@ onMounted(() => {
       toast({
         title: 'Success',
         description: 'Captcha token received',
-        variant: 'default'
+        variant: 'default',
       })
     },
     'error-callback': () => {
       toast({
         title: 'Error',
         description: 'Captcha token error',
-        variant: 'destructive'
+        variant: 'destructive',
       })
       captchaToken.value = ''
     },
@@ -112,10 +112,10 @@ onMounted(() => {
       toast({
         title: 'Warning',
         description: 'Captcha token expired',
-        variant: 'warning'
+        variant: 'warning',
       })
       captchaToken.value = ''
-    }
+    },
   })
 })
 
