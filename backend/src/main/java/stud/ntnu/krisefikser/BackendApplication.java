9e349a505b39be4a5f06ca42bd5189b76b617258
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-04-30 15:13:53 +0200

    chore: inform about selected profile

--- backend/src/main/java/stud/ntnu/krisefikser/BackendApplication.java
+++ backend/src/main/java/stud/ntnu/krisefikser/BackendApplication.java
@@ -1,9 +1,11 @@
 package stud.ntnu.krisefikser;
 
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import stud.ntnu.krisefikser.config.FrontendConfig;
 
 @SpringBootApplication
@@ -11,10 +13,12 @@ import stud.ntnu.krisefikser.config.FrontendConfig;
 public class BackendApplication {
 
   private final FrontendConfig frontendConfig;
+  private final Environment environment;
 
   public static void main(String[] args) {
     ConfigurableApplicationContext context = SpringApplication.run(BackendApplication.class, args);
     BackendApplication app = context.getBean(BackendApplication.class);
+    System.out.println("Active profiles: " + Arrays.toString(app.environment.getActiveProfiles()));
     System.out.println(
         "Application started with data seeding enabled. Database will be populated with sample data if empty.");
     System.out.println("Frontend URL: " + app.frontendConfig.getUrl());
