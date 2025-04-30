7552ee6b3bfc017d6cbf0efc802ac238059ec1c0
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-04-30 12:26:02 +0200

    Updated readme

--- backend/src/main/java/stud/ntnu/krisefikser/household/repository/HouseholdRepo.java
+++
@@ -1,9 +0,0 @@
-package stud.ntnu.krisefikser.household.repository;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import stud.ntnu.krisefikser.household.entity.Household;
-
-public interface HouseholdRepo extends JpaRepository<Household, UUID> {
-}
