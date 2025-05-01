047396c012e1e11dd216c4f8106ff04e90f5c43b
Jakob Huuse <jakob.huuse@gmail.com>
2025-05-01 15:12:07 +0200

    fix: merge conflict

--- backend/src/main/java/stud/ntnu/krisefikser/auth/config/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/stud/ntnu/krisefikser/auth/config/JwtAuthenticationEntryPoint.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.core.AuthenticationException;
@@ -12,7 +13,9 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+  private final ObjectMapper objectMapper;
   @Override
   public void commence(
       HttpServletRequest request,
