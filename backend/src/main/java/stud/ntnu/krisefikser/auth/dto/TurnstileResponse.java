1d34a9e4a8b5b4636394f1e31030d20e29535639
Kaamya Shinde <kaamyashinde@gmail.com>
2025-05-01 13:36:10 +0200

    refactor: add necessary imports that were missing

--- backend/src/main/java/stud/ntnu/krisefikser/auth/dto/TurnstileResponse.java
+++ backend/src/main/java/stud/ntnu/krisefikser/auth/dto/TurnstileResponse.java
@@ -1,9 +1,13 @@
 package stud.ntnu.krisefikser.auth.dto;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TurnstileResponse {
     private boolean success;
     private List<String> errorCodes;
