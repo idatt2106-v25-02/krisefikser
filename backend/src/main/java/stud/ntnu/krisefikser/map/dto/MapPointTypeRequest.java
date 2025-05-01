4743697dbec080c6106eb4c9ede188e693818469
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-05-01 15:14:57 +0200

    feat: create DTOs for map

---
+++ backend/src/main/java/stud/ntnu/krisefikser/map/dto/MapPointTypeRequest.java
@@ -0,0 +1,14 @@
+package stud.ntnu.krisefikser.map.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MapPointTypeRequest {
+
+}
