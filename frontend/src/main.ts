75454a8776b25ba2b0f93a0765dbfae6248502fe
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-05-05 14:28:45 +0200

    refactor: update frontend to correctly use refresh tokens

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,19 +9,24 @@ import { setupVueQuery } from './plugins/vue-query'
 import { WebSocketService } from '@/api/websocket/WebSocketService.ts'
 import accessibilityPlugin from './plugins/accessibility'
 import posthogPlugin from './plugins/posthog'
+import { useAuthStore } from './stores/useAuthStore'
+import { setStoreRef } from './api/storeRef'
 const app = createApp(App)
 export const webSocket = new WebSocketService()
 
 app.use(createPinia())
+setupVueQuery(app)
+
+const authStore = useAuthStore()
+setStoreRef(authStore)
+
 app.use(router)
 app.use(accessibilityPlugin)
 
-if (import.meta.env.VITE_POSTHOG_ENABLED === "true") {
+if (import.meta.env.VITE_POSTHOG_ENABLED === 'true') {
   app.use(posthogPlugin)
 } else {
-  console.log("PostHog is disabled")
+  console.log('PostHog is disabled')
 }
-
-setupVueQuery(app)
 
 app.mount('#app')
