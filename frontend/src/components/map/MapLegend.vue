72ea831381569e6fb73748b588ad3c4a7d6da043
Embret Roås <embret.roas@outlook.com>
2025-05-05 11:11:52 +0200

    home location is now visible on map

--- frontend/src/components/map/MapLegend.vue
+++ frontend/src/components/map/MapLegend.vue
@@ -27,7 +27,25 @@ function toggleMeetingPointCreation() {
     <div class="bg-white p-2.5 rounded-md shadow-md max-w-[280px]">
       <h3 class="mt-0 mb-2.5 text-base font-bold">Tegnforklaring</h3>
 
-
+      <div class="mb-2">
+        <div class="font-semibold mb-1">Lokasjon:</div>
+        <div class="flex items-center mb-1">
+          <div class="w-5 h-5 mr-2.5 bg-blue-500 rounded-full flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+            </svg>
+          </div>
+          <span>Min posisjon</span>
+        </div>
+        <div class="flex items-center mb-1">
+          <div class="w-5 h-5 mr-2.5 bg-red-500 rounded-full flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+            </svg>
+          </div>
+          <span>Hjem</span>
+        </div>
+      </div>
 
       <div class="mb-2">
         <div class="font-semibold mb-1">Hendelser:</div>
