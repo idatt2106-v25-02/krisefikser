ef83853bfd01697b2a156d7001a4d52c408bff7e
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-05-06 10:55:39 +0200

    pnpm orval; pnpm lint

--- frontend/src/api/generated/model/pageNotificationResponse.ts
+++ frontend/src/api/generated/model/pageNotificationResponse.ts
@@ -10,15 +10,15 @@ import type { SortObject } from './sortObject'
 import type { PageableObject } from './pageableObject'
 
 export interface PageNotificationResponse {
-  totalElements?: number
   totalPages?: number
+  totalElements?: number
   first?: boolean
   last?: boolean
   size?: number
   content?: NotificationResponse[]
   number?: number
   sort?: SortObject
-  numberOfElements?: number
   pageable?: PageableObject
+  numberOfElements?: number
   empty?: boolean
 }
