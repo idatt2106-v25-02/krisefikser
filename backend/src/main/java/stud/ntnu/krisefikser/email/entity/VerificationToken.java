1f4e22cc1ee8eb723f944285640f5dd35f167018
Conrad Tinius Osvik <55027923+ConradOsvik@users.noreply.github.com>
2025-05-09 20:38:19 +0200

    fix: minor linting errors (#378)

--- backend/src/main/java/stud/ntnu/krisefikser/email/entity/VerificationToken.java
+++ backend/src/main/java/stud/ntnu/krisefikser/email/entity/VerificationToken.java
@@ -28,25 +28,25 @@ import stud.ntnu.krisefikser.user.entity.User;
 @NoArgsConstructor
 @Table(name = "verification_tokens")
 public class VerificationToken {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
-    
-    @Column(nullable = false, unique = true)
-    private String token;
-    
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-    
-    @Column(nullable = false)
-    private LocalDateTime expiryDate;
-    
-    @Column(nullable = false)
-    private boolean used = false;
-    
-    public boolean isExpired() {
-        return LocalDateTime.now().isAfter(expiryDate);
-    }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(nullable = false, unique = true)
+  private String token;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(nullable = false)
+  private LocalDateTime expiryDate;
+
+  @Column(nullable = false)
+  private boolean used = false;
+
+  public boolean isExpired() {
+    return LocalDateTime.now().isAfter(expiryDate);
+  }
 }
