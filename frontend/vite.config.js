4d7ddcb03221da3a4ddd62e7ee355e2bfee4ab11
Embret Roås <embret.roas@outlook.com>
2025-05-09 22:23:29 +0200

    hotfix/styling

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), tailwindcss(), vueDevTools()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
