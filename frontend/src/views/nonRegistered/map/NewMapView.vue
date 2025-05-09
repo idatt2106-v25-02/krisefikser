7f0b30b3f1842e01ff87441ed4dbd2a10e3ed464
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-05-09 19:04:57 +0200

    Ready for event

--- frontend/src/views/nonRegistered/map/NewMapView.vue
+++ frontend/src/views/nonRegistered/map/NewMapView.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { useGetAllEvents } from '@/api/generated/event/event'
 import { useGetActiveHousehold } from '@/api/generated/household/household'
 import { useGetAllMapPointTypes } from '@/api/generated/map-point-type/map-point-type'
 import { useGetAllMapPoints } from '@/api/generated/map-point/map-point'
+import { useGetMeetingPoints } from '@/api/generated/meeting-points/meeting-points'
 import { createUserMarker } from '@/components/map/marker'
 import { createHouseholdMarker } from '@/components/map/marker/household'
 import { createHouseholdMemberMarker } from '@/components/map/marker/householdMember'
 import loadMapPoints from '@/components/map/marker/mapPoints'
+import { createMeetingPointMarker } from '@/components/map/marker/meetingPoint'
 import NewMapComponent from '@/components/map/NewMapComponent.vue'
 import { useMap } from '@/components/map/useMap'
 import { useAuthStore } from '@/stores/auth/useAuthStore'
@@ -15,12 +18,19 @@ import { computed, watch } from 'vue'
 const { data: mapPointsData, isLoading: isLoadingMapPoints } = useGetAllMapPoints()
 const { data: mapPointTypesData, isLoading: isLoadingMapPointTypes } = useGetAllMapPointTypes()
 const { data: activeHousehold, isLoading: isLoadingActiveHousehold } = useGetActiveHousehold()
+const { data: eventPointsData, isLoading: isLoadingEventPoints } = useGetAllEvents()
+
 const authStore = useAuthStore()
 
 const { initMap, addMarkers, onMapCreated, clearMarkers } = useMap()
 
 const isDataLoading = computed(() => {
-  return isLoadingMapPoints.value || isLoadingMapPointTypes.value || isLoadingActiveHousehold.value
+  return (
+    isLoadingMapPoints.value ||
+    isLoadingMapPointTypes.value ||
+    isLoadingActiveHousehold.value ||
+    isLoadingEventPoints.value
+  )
 })
 
 const renderNewMapPoints = async () => {
@@ -30,6 +40,12 @@ const renderNewMapPoints = async () => {
   if (mapPointsData.value && mapPointTypesData.value) {
     const mapMarkers = loadMapPoints(mapPointsData.value, mapPointTypesData.value)
     addMarkers(mapMarkers)
+  }
+
+  // Event points
+  if (eventPointsData.value) {
+    const eventMarkers = createEventMarkers(eventPointsData.value)
+    addMarkers(eventMarkers)
   }
 
   // Household realted markers
@@ -45,6 +61,20 @@ const renderNewMapPoints = async () => {
       .map((member) => createHouseholdMemberMarker(member))
       .filter((marker) => marker !== null)
     addMarkers(householdMembersMarkers)
+
+    // Meeting points markers
+    const { data: meetingPointsData, isLoading: isLoadingMeetingPoints } = useGetMeetingPoints(
+      activeHousehold.value.id,
+    )
+
+    watch(isLoadingMeetingPoints, () => {
+      if (meetingPointsData.value) {
+        const meetingPointsMarkers = meetingPointsData.value
+          .map((meetingPoint) => createMeetingPointMarker(meetingPoint))
+          .filter((marker) => marker !== null)
+        addMarkers(meetingPointsMarkers)
+      }
+    })
   }
 
   // User location marker
